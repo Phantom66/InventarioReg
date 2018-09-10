0aea2ceab75ee6c321c3d92c251a27ca5e1019a5
Phantom66 <leoturorellana@gmail.com>
2018-09-10 11:07:07 -0400

    Mantenimiento

--- src/com/inventario/con/ControladorInventario.java
+++ src/com/inventario/con/ControladorInventario.java
@@ -21,17 +21,7 @@ import com.inventario.dao.imp.PersonaDAOImpl;
 public class ControladorInventario extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ControladorInventario() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		//response.getWriter().append("Served at: ").append(request.getContextPath());
@@ -46,9 +36,7 @@ public class ControladorInventario extends HttpServlet {
 		miDispatcher.forward(request, response);
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+/
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);

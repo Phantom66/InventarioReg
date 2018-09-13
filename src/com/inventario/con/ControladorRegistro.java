feadf598fbb43a51d010fa3aa5528e823501996b
Phantom66 <leoturorellana@gmail.com>
2018-09-13 13:46:43 -0400

    Realizando un refactoring

--- src/com/inventario/con/ControladorRegistro.java
+++ src/com/inventario/con/ControladorRegistro.java
@@ -13,40 +13,37 @@ import com.inventario.bo.Producto;
 import com.inventario.dao.imp.PersonaDAOImpl;
 import com.inventario.dao.imp.ProductoDAOImpl;
 
-
-
 public class ControladorRegistro extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
 		RequestDispatcher despachador = null;
 		int cedula = Integer.parseInt(request.getParameter("cedula"));
 		String nombre = request.getParameter("nombre");
 		String apellidos = request.getParameter("apellido");
 		String telefono = request.getParameter("telefono");
-		
+
 		String producto = request.getParameter("producto");
 		String estatus = request.getParameter("status");
 		String descripcion = request.getParameter("descripcion");
-		
+
 		PersonaDAOImpl insertar = new PersonaDAOImpl();
-		
-		int id = insertar.insertar(new Persona(cedula,nombre,apellidos, telefono));
-		
+
+		// int id = insertar.insertar(new Persona(cedula,nombre,apellidos, telefono));
+
 		ProductoDAOImpl product = new ProductoDAOImpl();
-		product.insertar(new Producto(0, producto,estatus,descripcion), id);
-		
-		
-		
+		// product.insertar(new Producto(0, producto, estatus, descripcion), id);
+
 		despachador = request.getRequestDispatcher("./principal.do");
 		despachador.forward(request, response);
-		
+
 	}
 
 }
--- src/com/inventario/test/TestCRUDProducto.java
+++ src/com/inventario/test/TestCRUDProducto.java
@@ -7,43 +7,45 @@ import com.inventario.dao.imp.ProductoDAOImpl;
 
 /**
  * Clase creada para realizar prueba del CRUD Producto.
+ * 
  * @author phantom
  *
  */
 public class TestCRUDProducto {
 
 	public static void main(String[] args) {
-		
-		
-		Producto producto = new Producto(0,"Estante","En Buen Estado"," Oficina Principal");
+
+		// Producto producto = new Producto(0,"Estante","En Buen Estado"," Oficina
+		// Principal");
 		ProductoDAOImpl proDao = new ProductoDAOImpl();
-		
-		
-		//InsertandoProducto
-		//proDao.insertar(producto);
-		
-		Producto productoDos = new Producto(2, "Estante","En Buen Estado"," Oficina Principal");
-		//ActualizarProducto
-		//proDao.salvar(productoDos);
-		
-		//Borrar Producto
-		//proDao.borrar(productoDos);
-		
-		/*ListarTodosLosProductos
-			List<Producto> p = proDao.buscarTodos();
-			
-			for (Producto e: p) {
-				
-				System.out.println("Nombre: " + e.getNombre() + " Estatus: " + e.getEstatus());
-				
-			}
-		*/
-		
-		//Buscando Producto por Id
+
+		// InsertandoProducto
+		// proDao.insertar(producto);
+
+		// Producto productoDos = new Producto(2, "Estante", "En Buen Estado", " Oficina
+		// Principal");
+		// ActualizarProducto
+		// proDao.salvar(productoDos);
+
+		// Borrar Producto
+		// proDao.borrar(productoDos);
+
+		/*
+		 * ListarTodosLosProductos List<Producto> p = proDao.buscarTodos();
+		 * 
+		 * for (Producto e: p) {
+		 * 
+		 * System.out.println("Nombre: " + e.getNombre() + " Estatus: " +
+		 * e.getEstatus());
+		 * 
+		 * }
+		 */
+
+		// Buscando Producto por Id
 		Producto p = proDao.buscarPorClave("6");
-		
-		System.out.println(" Producot del id: "+p.getId()  + " Modelo: "+ p.getNombre());
-		
+
+		System.out.println(" Producot del id: " + p.getId() + " Modelo: " + p.getNombre());
+
 	}
 
 }

02db8de060a6344bbc1761f92d1959bd459ed1c0
Phantom66 <leoturorellana@gmail.com>
2018-09-21 09:36:34 -0400

    Realizando algunas anotaciones

--- src/com/inventario/dao/imp/PersonaDAOImpl.java
+++ src/com/inventario/dao/imp/PersonaDAOImpl.java
@@ -279,10 +279,49 @@ public class PersonaDAOImpl implements com.inventario.dao.PersonaDAO {
 		List<Persona>p = new ArrayList<Persona>();
 		Persona persona;
 		
-		//Verificar esta lógica.
+		/*
+		 * Con este cálculo puedo ir  corriendo las posiciones del registro
+		 * de mi tabla.
+		 * 
+		 * Ejemplo: Si quiero que me muestre de 5 en 5 los registro de mi tabla
+		 * , y quiero que me muestre un primer reglón, realizamos el cálculo:
+		 * 
+		 * pagActual = 1;
+		 * pagPerReg = 5;
+		 * start = ?;
+		 * 
+		 * start = (1*5)-5 = 0;
+		 * start = 0;
+		 * 
+		 * Con esto le indico a mi sentencia Sql(SELECT * FROM `persona` LIMIT 0 ,5)
+		 * que me muestre los cinco primeros registros de la tabla, al realiza el cálculo
+		 * nuevamente, cambiamos la página a 2:
+		 * 
+		 * pagActual = 2;
+		 * pagPerReg = 5;
+		 * start = ?;
+		 * 
+		 * start = (2*5)-5 = 5;
+		 * start = 5;
+		 * 
+		 * Con esto le indicamos a la sentencia Sql Sql(SELECT * FROM `persona` LIMIT 5 ,5)
+		 * que me muestre los tres segundo registro.
+		 * 
+		 * Esto sucesivamente a hasta mostra el final de los registro.
+		 * 
+		 * Cabe mencionar que la clave está como la palabra LIMIT en la sentencia SQL
+		 * muestra este tipo de información, el muestra la cantidad de valores
+		 * de acuerdo le indiquemos en el segundo parámetro.
+		 * 
+		 * si es de 3 en 3 o de 10 en 10, y va desde la posición de incio 0 hasta la posición
+		 * del registro que le indiquemos, ejemplo: 
+		 * posición 0(primeros registros a mostrar) y de acuerdo al segundo parámetro se empieza,
+		 * a contar, en caso de que sea 5 el segundo parámetro; 0,1,2,3,4, como si fuera una array luego se debe mover
+		 * a la posición 5; 5,6,7,8,9, y de esta manera mostraría los primeros 10 registro de 
+		 * 5 en 5 de forma consecutiva.
+		 * 
+		 */
 		 int start = (pagActual * perReg) - perReg;
-//
-//		System.out.println(start);
 		
 		try {
 			statement = this.conn.getConnection().prepareStatement("SELECT * FROM persona LIMIT ?,?");

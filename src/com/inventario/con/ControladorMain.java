559b2fb3a4e178cd7be6d7ddc9421e283b1d18a7
Phantom66 <leoturorellana@gmail.com>
2018-09-27 14:13:25 -0400

    Realizando refactoring con reflection Controlador

--- src/com/inventario/con/ControladorMain.java
+++ src/com/inventario/con/ControladorMain.java
@@ -14,66 +14,23 @@ public class ControladorMain extends HttpServlet {
 	
 	AccionPrincipal main = new AccionPrincipal();
 	String url;
-			
-	/**
-	 * 
-	 */
+
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		
-		String url = request.getServletPath();
+		url = request.getServletPath();
 		main.getAccion(url.substring(1, url.length() - 3), request, response);
-
-		// if (request.getServletPath().equals("/Principal.do")) {
-		//
-		// main.getPrincipal(request, response);
-		//
-		// } else if (request.getServletPath().equals("/Editar.do")) {
-		//
-		// main.getEditar(request, response);
-		//
-		// } else if (request.getServletPath().equals("/Crear.do")) {
-		//
-		// main.getCrear(request, response);
-		//
-		// } else if (request.getServletPath().equals("/Borrar.do")) {
-		//
-		// main.getBorrar(request, response);
-		//
-		// } else {
-		//
-		// System.out.println("No existe---" + url);
-		// }
 
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		
-		String url = request.getServletPath();
+		url = request.getServletPath();
 		main.getAccion(url.substring(1, url.length() - 3), request, response);
-		
-		// if (request.getServletPath().equals("/Principal.do")) {
-		//
-		// main.getPrincipal(request, response);
-		//
-		// }else if(request.getServletPath().equals("/Actualizar.do")){
-		//
-		// main.getActualizar(request, response);
-		//
-		// }else if(request.getServletPath().equals("/Registro.do")){
-		//
-		// main.getRegistrar(request, response);
-		//
-		// }else if(request.getServletPath().equals("/Reg_User.do")){
-		//
-		// main.getRegUser(request, response);
-		//
-		// }else {
-		// System.out.println("Estoy en la ---" + url);
-		// }
+
 	}
 	
 

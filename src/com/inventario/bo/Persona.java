0ec0110e58404961a765fca32d17ef8ba0004d57
Phantom66 <leoturorellana@gmail.com>
2018-11-13 16:52:22 -0400

    Relacionando objetos Verificar la forma que se relacionan los objetos, me da error

--- src/com/inventario/bo/Persona.java
+++ src/com/inventario/bo/Persona.java
@@ -1,42 +1,45 @@
 package com.inventario.bo;
 
-import java.util.Arrays;
+import java.util.List;
 
 public class Persona {
-	//private int id;
+
 	private int cedula;
 	private String nombre, apellido, telefono;
 	
-	private Producto [] productos;
-	private Perfil [] perfil;
+	private List <Producto> productos;
+	private List <Perfil> perfil;
 
-	public Persona() {};
-	
-	public Persona(int cedula, String nombre, String apellido, String telefono, Producto[] productos) {
+	public Persona() {}
+
+	public Persona(int cedula, String nombre, String apellido, String telefono) {
+		super();
+		this.cedula = cedula;
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.telefono = telefono;
+
+	}
+	public Persona(int cedula, String nombre, String apellido, String telefono, List<Producto> productos) {
+		super();
 		this.cedula = cedula;
 		this.nombre = nombre;
 		this.apellido = apellido;
 		this.telefono = telefono;
 		this.productos = productos;
 	}
-
-
-	public Persona(int cedula, String nombre, String apellido, String telefono) {
+	
+	public Persona(int cedula, String nombre, String apellido, String telefono, List<Producto> productos,
+			List<Perfil> perfil) {
+		super();
 		this.cedula = cedula;
 		this.nombre = nombre;
 		this.apellido = apellido;
 		this.telefono = telefono;
+		this.productos = productos;
+		this.perfil = perfil;
 	}
 
-//	
-//	public Persona(int id, int cedula, String nombre, String apellido, String telefono, Perfil[] perfil) {
-//		this.id = id;
-//		this.cedula = cedula;
-//		this.nombre = nombre;
-//		this.apellido = apellido;
-//		this.telefono = telefono;
-//		this.perfil = perfil;
-//	}
 
 
 
@@ -46,13 +49,14 @@ public class Persona {
 		int result = 1;
 		result = prime * result + ((apellido == null) ? 0 : apellido.hashCode());
 		result = prime * result + cedula;
-		//result = prime * result + id;
 		result = prime * result + ((nombre == null) ? 0 : nombre.hashCode());
-		result = prime * result + Arrays.hashCode(perfil);
-		result = prime * result + Arrays.hashCode(productos);
+		result = prime * result + ((perfil == null) ? 0 : perfil.hashCode());
+		result = prime * result + ((productos == null) ? 0 : productos.hashCode());
 		result = prime * result + ((telefono == null) ? 0 : telefono.hashCode());
 		return result;
 	}
+
+
 
 	@Override
 	public boolean equals(Object obj) {
@@ -70,16 +74,20 @@ public class Persona {
 			return false;
 		if (cedula != other.cedula)
 			return false;
-		// if (id != other.id)
-		// return false;
 		if (nombre == null) {
 			if (other.nombre != null)
 				return false;
 		} else if (!nombre.equals(other.nombre))
 			return false;
-		if (!Arrays.equals(perfil, other.perfil))
+		if (perfil == null) {
+			if (other.perfil != null)
+				return false;
+		} else if (!perfil.equals(other.perfil))
 			return false;
-		if (!Arrays.equals(productos, other.productos))
+		if (productos == null) {
+			if (other.productos != null)
+				return false;
+		} else if (!productos.equals(other.productos))
 			return false;
 		if (telefono == null) {
 			if (other.telefono != null)
@@ -89,16 +97,7 @@ public class Persona {
 		return true;
 	}
 
-//	public int getId() {
-//		
-//		return id;
-//	}
-//	
-//	public void setId(int id) {
-//		
-//		this.id = id;
-//	}
-	
+
 	public int getCedula() {
 		return cedula;
 	}
@@ -139,35 +138,31 @@ public class Persona {
 	}
 
 
-	public Producto[] getProductos() {
+	public List<Producto> getProductos() {
 		return productos;
 	}
 
 
-	public void setProductos(Producto[] productos) {
+	public void setProductos(List<Producto> productos) {
 		this.productos = productos;
 	}
 
-	
 
-	public Perfil[] getPerfil() {
+	public List<Perfil> getPerfil() {
 		return perfil;
 	}
 
 
-	public void setPerfil(Perfil[] perfil) {
+	public void setPerfil(List<Perfil> perfil) {
 		this.perfil = perfil;
 	}
-
 
 
 	@Override
 	public String toString() {
 		return "Persona [cedula=" + cedula + ", nombre=" + nombre + ", apellido=" + apellido + ", telefono=" + telefono
-				+ ", productos=" + Arrays.toString(productos) + "]";
+				+ ", productos=" + productos + ", perfil=" + perfil + "]";
 	}
-	
-	
 
 
 }

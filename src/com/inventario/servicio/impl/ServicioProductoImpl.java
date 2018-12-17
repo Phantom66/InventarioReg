5a3d1a8f1fcd9893d4b9c1451722e98af18cbf52
Phantom66 <leoturorellana@gmail.com>
2018-12-17 15:54:46 -0400

    Factoria Spring Producto

--- src/com/inventario/servicio/impl/ServicioProductoImpl.java
+++ src/com/inventario/servicio/impl/ServicioProductoImpl.java
@@ -2,10 +2,11 @@ package com.inventario.servicio.impl;
 
 import java.util.List;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 import com.inventario.bo.Producto;
-import com.inventario.dao.DAOFactory;
 import com.inventario.dao.ProductoDAO;
-import com.inventario.dao.imp.DAOAbstractFactory;
 import com.inventario.servicio.ServicioProducto;
 
 public class ServicioProductoImpl implements ServicioProducto {
@@ -13,8 +14,12 @@ public class ServicioProductoImpl implements ServicioProducto {
 	ProductoDAO producto = null;
 
 	public ServicioProductoImpl() {
-		DAOFactory factoria =  DAOAbstractFactory.getInstance();
-		producto = factoria.getProductoDAO();
+		
+		//DAOFactory factoria =  DAOAbstractFactory.getInstance();
+		
+		ApplicationContext factoria = new ClassPathXmlApplicationContext("contextoAplicacion.xml");
+		
+		producto = (ProductoDAO)factoria.getBean("productoDAO");
 		
 	}
 
